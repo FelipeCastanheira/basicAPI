de114ee004287b77743e8415d36996cf1cc47b5e
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2022-04-23 10:26:45 -0300

    index file

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const errorHandler = require('./middlewares/errorHandler');
+const router = require('./router');
+const app = express();
+app.use(express.json());
+
+app.use(router);
+app.use(errorHandler);
+
+app.listen(3000, () => console.log('Ouvindo na porta 3000'));
