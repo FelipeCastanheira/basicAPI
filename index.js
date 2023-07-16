78b1137ea19d984a4cf530ed861359baa2924f63
Felipe <castanheirafc2@gmail.com>
2023-07-16 17:23:23 -0300

    commit

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ app.use(express.json());
 app.use(router);
 app.use(errorHandler);
 
-app.listen(3000, () => console.log('Ouvindo na porta 3000'));
+app.listen(3000, () => console.log('Ouvindo na porta 3000!'));
